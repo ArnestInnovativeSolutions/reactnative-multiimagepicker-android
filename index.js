4a1a1a2e46c6d3b89673b5d8454a7cdf642ef4f7
Shranya M <Sharanya@arnest.in>
2019-09-25 10:37:37 +0530

    documented

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import React from 'react';
 
 import {NativeModules} from 'react-native';
+/**
+ * We export the native interface in this way to give easy shared access to 
+ * JavaScript code 
+ */
 export default NativeModules.MultipleImagePicker;
